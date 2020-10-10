1279b05538e21fe21f203bbe539c693fd45a791b
Israel Huallpara <israelhuallpara@gmail.com>
2020-10-10 09:54:48 -0400

    Menu de chat

--- redis/chat/cliente.java
+++ redis/chat/cliente.java
@@ -11,11 +11,21 @@ public class cliente {
         //Para la autendicación en redis
         jedis.auth("dSSj6jmUk1FOanYEtwKZPy8CsSfOtZcG");
         //Inicia programa
-        solicitaNick();
+        do {
+            solicitaNick();
+            //Si el nickname ya está utilizado, solicitar nuevamente.
+        } while (miNick == null);
+        //Indicar al usuario las conexiones en sala.
+        System.out.println("Hay "+cntConectados()+" personas en sala...");
+        do {
+            menu();
+
+        } while(opciones()!=0);
+        salir();
     }
     public static void solicitaNick(){
         //Solicitar un nickname
-        System.out.print("Ingrese su nickname: ");
+        System.out.print("\nIngrese su nickname: ");
         String nuevoNickname = leer.nextLine();
         Long resultado = jedis.sadd("nicks", nuevoNickname);
         if(resultado == 0){
@@ -24,7 +34,26 @@ public class cliente {
             miNick = nuevoNickname;
             System.out.println("Hola, "+miNick);
         }
-        //Si el nickname ya está utilizado, solicitar nuevamente.
 
+    }
+    public static long cntConectados(){
+        return jedis.scard("nicks");
+    }
+    public static void menu(){
+        System.out.print("Quieres: \n" +
+                "1) Ver quíen está conectad@\n" + //SMEMBERS
+                "2) Enviar mensaje\n" +
+                "3) Ver estados\n" +
+                "4) Contacto directo con\n" +
+                "5) Ver mensajes\n" +
+                "0) Salir\n" +
+                "Seleccione una opción: ");
+    }
+    public static int opciones(){
+        int opc = leer.nextInt();
+        return opc;
+    }
+    public static void salir(){
+        System.out.println("Gracias, vuelve pronto....");
     }
 }
